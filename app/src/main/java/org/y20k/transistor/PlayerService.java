0d3774d552f2be26435a76c80277ddeae1cd43a1
y20k <mail@y20k.org>
2016-04-18 10:30:19 +0200

    Extraction metadata is now handled by MetadataHelper

--- app/src/main/java/org/y20k/transistor/PlayerService.java
+++ app/src/main/java/org/y20k/transistor/PlayerService.java
@@ -23,7 +23,6 @@ import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.wifi.WifiManager;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.preference.PreferenceManager;
@@ -32,18 +31,10 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.widget.Toast;
 
+import org.y20k.transistor.helpers.MetadataHelper;
 import org.y20k.transistor.helpers.NotificationHelper;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
 
 
 /**
@@ -66,22 +57,22 @@ public final class PlayerService extends Service implements
     private static final String ACTION_STOP = "org.y20k.transistor.action.STOP";
     private static final String ACTION_PLAYBACK_STARTED = "org.y20k.transistor.action.PLAYBACK_STARTED";
     private static final String ACTION_PLAYBACK_STOPPED = "org.y20k.transistor.action.PLAYBACK_STOPPED";
+    private static final String ACTION_METADATA_CHANGED = "org.y20k.transistor.action.METADATA_CHANGED";
+    private static final String EXTRA_METADATA = "METADATA";
     private static final String EXTRA_STREAM_URI = "STREAM_URI";
     private static final String PREF_PLAYBACK = "prefPlayback";
     private static final int PLAYER_SERVICE_NOTIFICATION_ID = 1;
-    private static final String SHOUTCAST_STREAM_TITLE_HEADER = "StreamTitle='";
 
 
     /* Main class variables */
+    private MetadataHelper mMetadataHelper;
     private AudioManager mAudioManager;
     private MediaPlayer mMediaPlayer;
     private String mStreamUri;
     private boolean mPlayback;
     private int mPlayerInstanceCounter;
     private HeadphoneUnplugReceiver mHeadphoneUnplugReceiver;
-    private WifiManager.WifiLock mWifiLock;
-    private Socket mProxyConnection = null;
-    private boolean mProxyRunning = false;
+//    private WifiManager.WifiLock mWifiLock;
 
 
     /* Constructor (default) */
@@ -102,6 +93,20 @@ public final class PlayerService extends Service implements
         IntentFilter headphoneUnplugIntentFilter = new IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY);
         mHeadphoneUnplugReceiver = new HeadphoneUnplugReceiver();
         registerReceiver(mHeadphoneUnplugReceiver, headphoneUnplugIntentFilter);
+
+        // RECEIVER: station metadata has changed
+        BroadcastReceiver metadataChangedReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.hasExtra(EXTRA_METADATA)) {
+                    // update notification
+                    NotificationHelper.setStationName(intent.getStringExtra(EXTRA_METADATA));
+                    NotificationHelper.createNotification(PlayerService.this);
+                }
+            }
+        };
+        IntentFilter metadataChangedIntentFilter = new IntentFilter(ACTION_METADATA_CHANGED);
+        LocalBroadcastManager.getInstance(this).registerReceiver(metadataChangedReceiver, metadataChangedIntentFilter);
 
         // TODO Listen for headphone button
         // Use MediaSession
@@ -379,9 +384,9 @@ public final class PlayerService extends Service implements
         mMediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK); // needs android.permission.WAKE_LOCK
 
         try {
-            mMediaPlayer.setDataSource(createShoutcastProxyConnection());
+            mMetadataHelper = new MetadataHelper(mStreamUri);
+            mMediaPlayer.setDataSource(mMetadataHelper.getShoutcastProxy());
             mMediaPlayer.prepareAsync();
-            Log.v(LOG_TAG, "setting: " + mStreamUri);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -397,7 +402,8 @@ public final class PlayerService extends Service implements
             mMediaPlayer.reset();
             mMediaPlayer.release();
             mMediaPlayer = null;
-            closeShoutcastProxyConnectionAsync();
+
+            mMetadataHelper.closeShoutcastProxyConnection();
         }
     }
 
@@ -483,166 +489,5 @@ public final class PlayerService extends Service implements
     /**
      * End of inner class
      */
-
-    /* Connect to the server, and create a listening socket on localhost,
-       to stream data into the MediaPlayer, and to pull Shoutcast metadata from the stream.
-       Returns localhost URL for MediaPlayer to connect to.
-       Shoutcast metadata described here: http://www.smackfu.com/stuff/programming/shoutcast.html */
-    private String createShoutcastProxyConnection() {
-        closeShoutcastProxyConnection();
-        mProxyRunning = true;
-        final StringBuffer uri = new StringBuffer();
-
-        try {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    Socket proxy = null;
-                    URLConnection connection = null;
-
-                    try {
-                        final ServerSocket proxyServer = new ServerSocket(0, 1, InetAddress.getLocalHost());
-                        uri.append("http://localhost:" + String.valueOf(proxyServer.getLocalPort()) + "/");
-                        Log.v(LOG_TAG, "createProxyConnection: " + uri.toString());
-
-                        proxy = proxyServer.accept();
-                        mProxyConnection = proxy;
-                        proxyServer.close();
-
-                        connection = new URL(mStreamUri).openConnection();
-
-                        shoutcastProxyReaderLoop(proxy, connection);
-
-                    } catch (Exception e) {}
-
-                    mProxyRunning = false;
-
-                    try {
-                        if (connection != null) {
-                            ((HttpURLConnection)connection).disconnect();
-                        }
-                    } catch (Exception ee) {}
-
-                    try {
-                        if (proxy != null && !proxy.isClosed()) {
-                            proxy.close();
-                        }
-                    } catch (Exception eee) {}
-                }
-            }).start();
-
-            while (uri.length() == 0) {
-                try {
-                    Thread.sleep(10);
-                } catch (Exception e) {}
-            }
-            return uri.toString();
-        } catch (Exception e) {
-            Log.e(LOG_TAG, "createProxyConnection: cannot create new listening socket on localhost: " + e.toString());
-            mProxyRunning = false;
-            return "";
-        }
-    }
-
-    private void closeShoutcastProxyConnectionAsync() {
-        try {
-            if (mProxyConnection != null && !mProxyConnection.isClosed()) {
-                mProxyConnection.close(); // Terminate proxy thread loop
-            }
-        } catch (Exception e) {}
-    }
-
-    private void closeShoutcastProxyConnection() {
-        try {
-            while (mProxyRunning && mProxyConnection == null) {
-                Thread.sleep(50); // Wait for proxyServer to initialize
-            }
-            closeShoutcastProxyConnectionAsync();
-            mProxyConnection = null;
-            while (mProxyRunning) {
-                Thread.sleep(50); // Wait for thread to finish
-            }
-        } catch(Exception e) {}
-    }
-
-    private void shoutcastProxyReaderLoop(Socket proxy, URLConnection connection) throws IOException {
-
-        connection.setConnectTimeout(5000);
-        connection.setReadTimeout(5000);
-        connection.setRequestProperty("Icy-MetaData", "1");
-        connection.connect();
-
-        InputStream in = connection.getInputStream();
-
-        OutputStream out = proxy.getOutputStream();
-        out.write( ("HTTP/1.0 200 OK\r\n" +
-                "Pragma: no-cache\r\n" +
-                "Content-Type: " + connection.getContentType() +
-                "\r\n\r\n").getBytes(StandardCharsets.UTF_8));
-
-        byte buf[] = new byte[16384]; // One second of 128kbit stream
-        int count = 0;
-        int total = 0;
-        int metadataSize = 0;
-        final int metadataOffset = connection.getHeaderFieldInt("icy-metaint", 0);
-        int bitRate = Math.max(connection.getHeaderFieldInt("icy-br", 128), 32);
-        Log.v(LOG_TAG, "createProxyConnection: connected, icy-metaint " + metadataOffset + " icy-br " + bitRate);
-        while (true) {
-            count = Math.min(in.available(), buf.length);
-            if (count <= 0) {
-                count = Math.min(bitRate * 64, buf.length); // Buffer half-second of stream data
-            }
-            if (metadataOffset > 0) {
-                count = Math.min(count, metadataOffset - total);
-            }
-
-            count = in.read(buf, 0, count);
-            if (count == 0) {
-                continue;
-            }
-            if (count < 0) {
-                break;
-            }
-
-            out.write(buf, 0, count);
-
-            total += count;
-            if (metadataOffset > 0 && total >= metadataOffset) {
-                // Read metadata
-                total = 0;
-                count = in.read();
-                if (count < 0) {
-                    break;
-                }
-                count *= 16;
-                metadataSize = count;
-                if (metadataSize == 0) {
-                    continue;
-                }
-                // Maximum metadata length is 4080 bytes
-                total = 0;
-                while (total < metadataSize) {
-                    count = in.read(buf, total, count);
-                    if (count < 0) {
-                        break;
-                    }
-                    if (count == 0) {
-                        continue;
-                    }
-                    total += count;
-                    count = metadataSize - total;
-                }
-                total = 0;
-                String[] metadata = new String(buf, 0, metadataSize, StandardCharsets.UTF_8).split(";");
-                for (String s : metadata) {
-                    if (s.indexOf(SHOUTCAST_STREAM_TITLE_HEADER) == 0 && s.length() >= SHOUTCAST_STREAM_TITLE_HEADER.length() + 1) {
-                        // Update notification
-                        NotificationHelper.setStationName(s.substring(SHOUTCAST_STREAM_TITLE_HEADER.length(), s.length() - 1));
-                        NotificationHelper.createNotification(PlayerService.this);
-                    }
-                }
-            }
-        }
-    }
 
 }
