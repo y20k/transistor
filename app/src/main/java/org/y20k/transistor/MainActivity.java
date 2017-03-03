e63a77cb27f8f6fb6c3f6f1e4f3ea53ae8ed3104
malah-code <tarekelmallah2@gmail.com>
2017-03-03 11:13:56 -0500

    Some Material Design -Change Theme to NoActionBar -Use toolbar as ActionBar instead of old ActionBar -Main Activity to implements NavigationView.OnNavigationItemSelectedListener -Add Drawer to the Layout -Change Color Styles

--- app/src/main/java/org/y20k/transistor/MainActivity.java
+++ app/src/main/java/org/y20k/transistor/MainActivity.java
@@ -24,10 +24,16 @@ import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -41,7 +47,7 @@ import java.io.File;
 /**
  * MainActivity class
  */
-public final class MainActivity extends AppCompatActivity {
+public final class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener  {
 
     /* Define log tag */
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -71,8 +77,46 @@ public final class MainActivity extends AppCompatActivity {
 
         // initialize broadcast receivers
         initializeBroadcastReceivers();
+
+        //toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
     @Override
     protected void onResume() {
