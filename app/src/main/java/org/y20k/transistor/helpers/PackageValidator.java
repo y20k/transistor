5b4e779e7c7b7c143f78d27e595ee23dfc167917
emaiannone <emaiannone96@gmail.com>
2020-01-18 12:29:13 +0100

    Refactor Member Ignoring Method smell in PackageValidator class

--- app/src/main/java/org/y20k/transistor/helpers/PackageValidator.java
+++ app/src/main/java/org/y20k/transistor/helpers/PackageValidator.java
@@ -53,38 +53,33 @@ public class PackageValidator {
                 R.xml.allowed_media_browser_callers));
     }
 
-    private Map<String, ArrayList<CallerInfo>> readValidCertificates(XmlResourceParser parser) {
-        HashMap<String, ArrayList<CallerInfo>> validCertificates = new HashMap<>();
-        try {
-            int eventType = parser.next();
-            while (eventType != XmlResourceParser.END_DOCUMENT) {
-                if (eventType == XmlResourceParser.START_TAG
-                        && parser.getName().equals("signing_certificate")) {
-
-                    String name = parser.getAttributeValue(null, "name");
-                    String packageName = parser.getAttributeValue(null, "package");
-                    boolean isRelease = parser.getAttributeBooleanValue(null, "release", false);
-                    String certificate = parser.nextText().replaceAll("\\s|\\n", "");
-
-                    CallerInfo info = new CallerInfo(name, packageName, isRelease);
-
-                    ArrayList<CallerInfo> infos = validCertificates.get(certificate);
-                    if (infos == null) {
-                        infos = new ArrayList<>();
-                        validCertificates.put(certificate, infos);
-                    }
-                    LogHelper.v(LOG_TAG, "Adding allowed caller: " + info.name +
-                            " package=" + info.packageName + " release=" + info.release +
-                            " certificate=" + certificate);
-                    infos.add(info);
-                }
-                eventType = parser.next();
-            }
-        } catch (XmlPullParserException | IOException e) {
-            LogHelper.e(LOG_TAG, "Could not read allowed callers from XML.");
-        }
-        return validCertificates;
-    }
+    private static Map<String, ArrayList<CallerInfo>> readValidCertificates(XmlResourceParser parser) {
+		HashMap<String, ArrayList<CallerInfo>> validCertificates = new HashMap<>();
+		try {
+			int eventType = parser.next();
+			while (eventType != XmlResourceParser.END_DOCUMENT) {
+				if (eventType == XmlResourceParser.START_TAG && parser.getName().equals("signing_certificate")) {
+					String name = parser.getAttributeValue(null, "name");
+					String packageName = parser.getAttributeValue(null, "package");
+					boolean isRelease = parser.getAttributeBooleanValue(null, "release", false);
+					String certificate = parser.nextText().replaceAll("\\s|\\n", "");
+					CallerInfo info = new CallerInfo(name, packageName, isRelease);
+					ArrayList<CallerInfo> infos = validCertificates.get(certificate);
+					if (infos == null) {
+						infos = new ArrayList<>();
+						validCertificates.put(certificate, infos);
+					}
+					LogHelper.v(LOG_TAG, "Adding allowed caller: " + info.name + " package=" + info.packageName
+							+ " release=" + info.release + " certificate=" + certificate);
+					infos.add(info);
+				}
+				eventType = parser.next();
+			}
+		} catch (XmlPullParserException | IOException e) {
+			LogHelper.e(LOG_TAG, "Could not read allowed callers from XML.");
+		}
+		return validCertificates;
+	}
 
     /**
      * @return false if the caller is not authorized to get data from this MediaBrowserService
